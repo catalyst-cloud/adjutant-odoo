0f56be5e6d265550f0bd3c86d7ce6f1c0fbe33ea
adrian-turjak <adriant@catalyst.net.nz>
2016-08-16 10:10:50 +1200

    fixing business requirements

--- odoo_actions/serializers.py
+++ odoo_actions/serializers.py
@@ -60,17 +60,13 @@ class NewClientSignUpSerializer(serializers.Serializer):
     signup_type = serializers.ChoiceField(
         choices=['individual', 'business'])
 
-    # Indidividual or Primary business contact
+    # Indidividual or business
     first_name = serializers.CharField(max_length=100)
     last_name = serializers.CharField(max_length=100)
     email = serializers.EmailField()
-
-    # Individual or Company mobile contact
-    # if company, should this phone be associated with
-    # the primary contact, or the company?
     phone = serializers.CharField(max_length=100)
-
     toc_agreed = serializers.BooleanField()
+    discount_code = serializers.CharField(max_length=100, default="")
 
     # business details
     company_name = serializers.CharField(max_length=100, required=False)
@@ -82,7 +78,6 @@ class NewClientSignUpSerializer(serializers.Serializer):
     country = serializers.CharField(max_length=100, required=False)
     payment_method = serializers.ChoiceField(
         choices=['invoice', 'credit_card'], required=False)
-
     bill_first_name = serializers.CharField(max_length=100, required=False)
     bill_last_name = serializers.CharField(max_length=100, required=False)
     bill_email = serializers.EmailField(required=False)
@@ -94,8 +89,11 @@ class NewClientSignUpSerializer(serializers.Serializer):
     bill_postal_code = serializers.CharField(max_length=100, required=False)
     bill_country = serializers.CharField(max_length=100, required=False)
 
-    # both business and individual
-    discount_code = serializers.CharField(max_length=100, default="")
+    def _check_field(self, errors, field, data):
+        value = data.get(field)
+        if not value:
+            errors.append(field)
+        return value
 
     def validate(self, data):
 
@@ -105,28 +103,29 @@ class NewClientSignUpSerializer(serializers.Serializer):
 
         if data['signup_type'] == 'business':
 
-            if not data.get('payment_method'):
-                raise serializers.ValidationError(
-                    "Payment method required for business signups.")
+            missing_fields = []
 
-            try:
-                company_name = data['company_name']
-                first_name = data['first_name']
-                last_name = data['last_name']
-                email = data['email']
-                phone = data['phone']
-                address_1 = data['address_1']
-                address_2 = data.get('address_2')  # Not required
-                city = data['city']
-                region = data.get('region')  # Not required
-                postal_code = data['postal_code']
-                country = data['country']
+            self._check_field(missing_fields, 'payment_method', data)
+            self._check_field(missing_fields, 'company_name', data)
 
-            # if any of the required primary address fields are missing
-            # we throw a validation error.
-            except KeyError:
+            first_name = self._check_field(missing_fields, 'first_name', data)
+            last_name = self._check_field(missing_fields, 'last_name', data)
+            email = self._check_field(missing_fields, 'email', data)
+            phone = self._check_field(missing_fields, 'phone', data)
+            address_1 = self._check_field(missing_fields, 'address_1', data)
+            address_2 = data.get('address_2')  # Not required
+            data['address_2'] = address_2
+            city = self._check_field(missing_fields, 'city', data)
+            region = data.get('region')  # Not required
+            data['region'] = region
+            postal_code = self._check_field(
+                missing_fields, 'postal_code', data)
+            country = self._check_field(missing_fields, 'country', data)
+
+            if missing_fields:
                 raise serializers.ValidationError(
-                    "Address info required for business signups.")
+                    "These fields are required for businesses: %s" %
+                    missing_fields)
 
             bill_first_name = data.get('bill_first_name')
             bill_last_name = data.get('bill_last_name')
